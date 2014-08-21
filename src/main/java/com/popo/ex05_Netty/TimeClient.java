3e0f799ee8da575d11e1ebea19569704d55832c4
Popo-lth <liutianhao001@sina.com>
2014-08-21 22:26:50 +0800

    补全TimeClient代码
    补全TimeClient代码。

--- src/main/java/com/popo/ex05_Netty/TimeClient.java
+++ src/main/java/com/popo/ex05_Netty/TimeClient.java
@@ -8,6 +8,16 @@
  */
 
 package com.popo.ex05_Netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * ClassName:TimeClient <br/>
  * Function: TODO ADD FUNCTION. <br/>
@@ -19,10 +29,46 @@ package com.popo.ex05_Netty;
  * @see
  */
 public class TimeClient {
+    
+    public void connect(int port, String  host) throws Exception {
+        // 配置客户端 NIO 线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+            .option(ChannelOption.TCP_NODELAY, true)
+            .handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel arg0) throws Exception {
+                    arg0.pipeline().addLast(new TimeClientHandler());
+                }
+                
+            });
+            
+            // 发起异步连接操作
+            ChannelFuture f = b.connect(host, port).sync();
+            
+            // 等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出, 释放 NIO 线程组
+            group.shutdownGracefully();
+        }
+    }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub 
-
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        
+        new TimeClient();
     }
 
 }
--- src/main/java/com/popo/ex05_Netty/TimeClientHandler.java
+++ src/main/java/com/popo/ex05_Netty/TimeClientHandler.java
@@ -9,8 +9,12 @@
 
 package com.popo.ex05_Netty;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+
+import java.util.logging.Logger;
 
 /**
  * ClassName:TimeClientHandler <br/>
@@ -24,29 +28,37 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class TimeClientHandler extends ChannelHandlerAdapter {
 
+    private static final Logger logger = Logger.getLogger(TimeClientHandler.class.getName());
+    private final ByteBuf firstMessage;
+    
     public TimeClientHandler() {
         // TODO Auto-generated constructor stub
-
+        byte [] req = "QUERY TIME ORDEY".getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
     }
     
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        		// TODO Auto-generated method stub super.channelActive(ctx);
-        	
+        ctx.writeAndFlush(firstMessage);
     }
     
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg)
             throws Exception {
-        		// TODO Auto-generated method stub super.channelRead(ctx, msg);
-        	
+        ByteBuf buf = (ByteBuf) msg;
+        byte [] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is : " + body);
     }
     
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
             throws Exception {
-        		// TODO Auto-generated method stub super.exceptionCaught(ctx, cause);
-        	
+        // 释放资源
+        logger.warning("Unexpected exception from downstream : " + cause.getMessage());
+        ctx.close();
     }
 }
 
